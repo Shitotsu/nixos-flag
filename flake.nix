e3b9188732727fd7b4034f0b3e3a100f26e15060
Lambda <87592388+Shitotsu@users.noreply.github.com>
2023-09-30 11:55:08 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,21 +2,16 @@
   description = "A very basic flake";
 
   outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux =
+    devShells.x86_64-linux =
       let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.mkShell {
-        buildInputs = [
-          pkgs.gcc.cc.lib
-        ];
-        shellHook = ''
-          ls -lah
-          ls -lah /
-          ls -lah /flag
-          cat /flag
-          echo hi
-          LD_LIBRARY_PATH=${pkgs.gcc.cc.lib}/lib/
-        '';
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in
+      {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixos-rebuild
+          ];
+        };
       };
   };
 }
