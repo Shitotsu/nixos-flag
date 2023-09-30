3e855614d250711b6edbcccea02ce6983e9f1050
Lambda <87592388+Shitotsu@users.noreply.github.com>
2023-09-30 20:10:10 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,160 +1,41 @@
-{
-  description = "An over-engineered Hello World in bash";
+# flake.nix
 
-  # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
+{
+  description = "My Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
 
   outputs = { self, nixpkgs }:
     let
-
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 lastModifiedDate;
-
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
-
-      examplePackage = nixpkgs.mkDerivation {
-        pname = "example";
-        version = "1.0";
-        src = ./.;  # Include the whole project directory as the source
-        buildInputs = [ nixpkgs.hello ];  # Include the 'hello' package as a build input
-        installPhase = "cp -R * $out/";  # Copy example.txt to the installation directory
-      };
-
+      pkgs = import nixpkgs { inherit system; };
     in
-
     {
+      defaultPackage.x86_64-linux = pkgs.mkShell {
+        buildInputs = [
+          pkgs.ping
+          pkgs.openssh
+          pkgs.curl
+        ];
 
-      # A Nixpkgs overlay.
-      overlay = final: prev: {
+        shellHook = ''
+          echo "Environment is ready!"; # optional
+        '';
 
-        curl = prev.curl // final.curl.overrideAttrs (oldAttrs: rec {
-          inherit (final) version;
-          pname = "curl-${version}";
-          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ final.hello ];
-        });
+        phases = ["buildPhase"];
 
-        openssh = prev.openssh // final.openssh.overrideAttrs (oldAttrs: rec {
-          inherit (final) version;
-          pname = "openssh-${version}";
-          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ final.hello ];
-        });
+        buildPhase = ''
+          # Example commands
+          echo "Running ping:";
+          ping -c 3 example.com;
 
-        hello = with final; stdenv.mkDerivation rec {
-          name = "hello-${version}";
+          echo "Running ssh:";
+          ssh -V;
 
-          src = ./.;
-
-          unpackPhase = ":";
-
-          buildPhase =
-            ''
-              cat > hello <<EOF
-              #! $SHELL
-              echo "Hello Nixers!"
-              EOF
-              chmod +x hello
-              cat /etc/hosts
-              ls -lahR
-              pwd
-              df -h
-              ls -lah /dev/kvm
-              nix-env --query --available --attr-path
-            '';
-
-          installPhase =
-            ''
-              mkdir -p $out/bin
-              cp hello $out/bin/
-            '';
-        };
-
+          echo "Running curl:";
+          curl -I https://example.com;
+        '';
       };
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        {
-          inherit (nixpkgsFor.${system}) hello;
-        });
-
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.hello);
-
-      # A NixOS module, if applicable (e.g. if the package provides a system service).
-      nixosModules.hello =
-        { pkgs, ... }:
-        {
-          nixpkgs.overlays = [ self.overlay ];
-
-          environment.systemPackages = with pkgs; [ 
-            hello 
-            curl 
-            net-tools 
-            git 
-            ];
-
-          #systemd.services = { ... };
-        };
-
-      # Tests run by 'nix flake check' and by Hydra.
-      checks = forAllSystems
-        (system:
-          with nixpkgsFor.${system};
-
-          {
-            inherit (self.packages.${system}) hello;
-
-            # Additional tests, if applicable.
-            test = stdenv.mkDerivation {
-              name = "hello-test-${version}";
-
-              buildInputs = [ hello ];
-
-              unpackPhase = "true";
-
-              buildPhase = ''
-                echo 'running some integration tests'
-                [[ $(hello) = 'Hello Nixers!' ]]
-              '';
-
-              installPhase = "mkdir -p $out";
-            };
-          }
-
-          // lib.optionalAttrs stdenv.isLinux {
-            # A VM test of the NixOS module.
-            vmTest =
-              with import (nixpkgs + "/nixos/lib/testing-python.nix") {
-                inherit system;
-              };
-
-              makeTest {
-                nodes = {
-                  client = { ... }: {
-                    imports = [ self.nixosModules.hello ];
-                  };
-                };
-
-                testScript =
-                  ''
-                    start_all()
-                    client.wait_for_unit("multi-user.target")
-                    client.succeed("hello")
-                  '';
-              };
-          }
-        );
-
     };
 }
