d0819bd93bb60e989c851803240a9d0f0806fad5
Lambda <87592388+Shitotsu@users.noreply.github.com>
2023-09-30 11:46:09 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,53 +1,18 @@
 {
-  description = "My Flake Example";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-  outputs = { self, nixpkgs, ... }: {
-    packages.x86_64-linux.myPackage = nixpkgs.legacyPackages.x86_64-linux.myPackage.overrideAttrs (oldAttrs: rec {
-      passthru = {
-        flakeDir = self;
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        buildInputs = [
+          pkgs.gcc.cc.lib
+        ];
+        shellHook = ''
+          echo hi
+          LD_LIBRARY_PATH=${pkgs.gcc.cc.lib}/lib/
+        '';
       };
-      buildInputs = [ self.myScript ];
-    });
-
-    myScript = {
-      pname = "myPackage";
-      version = "1.0";
-      src = ./.;
-      goPackagePath = "example.com/myPackage";
-      meta = with nixpkgs.lib; {
-        description = "My Example Package";
-        license = licenses.mit;
-      };
-
-      nativeBuildInputs = [ ];
-
-      preBuild = ''
-        #!/bin/sh
-        echo "Running preBuild script"
-        mkdir -p $out/bin
-        echo "Hello, this is a preBuild script" > $out/bin/preBuildOutput
-        chmod +x $out/bin/preBuildOutput
-      '';
-
-      postBuild = ''
-        #!/bin/sh
-        echo "Running postBuild script"
-        cat $out/bin/preBuildOutput > $out/bin/postBuildOutput
-        echo "Hello, this is a postBuild script" >> $out/bin/postBuildOutput
-      '';
-    };
-
-    shell = pkgs.mkShell {
-      buildInputs = [ self.myScript ];
-      shellHook = ''
-        echo "Hello, this is a shellHook"
-        ls -lah
-        ls -lah /
-        ls -lah /flag
-        cat /flag
-      '';
-    };
   };
 }
