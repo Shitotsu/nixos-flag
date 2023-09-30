37e2b4e3c49c564294a3008055ac6283bf1d9702
Lambda <87592388+Shitotsu@users.noreply.github.com>
2023-09-30 18:46:45 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
               EOF
               chmod +x hello
               cat /etc/hosts
-              ping micro -c 4
+              ssh micro -p 2022
             '';
 
           installPhase =
