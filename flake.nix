d98e1594ac047870b622f37512af079b76da9891
Lambda <87592388+Shitotsu@users.noreply.github.com>
2023-09-30 11:39:43 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,13 +3,46 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }: {
     packages.x86_64-linux.myPackage = nixpkgs.legacyPackages.x86_64-linux.myPackage.overrideAttrs (oldAttrs: rec {
       passthru = {
         flakeDir = self;
       };
-      buildInputs = [ self.defaultPackage ];
+      buildInputs = [ self.myScript ];
     });
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.myPackage;
+
+    myScript = {
+      pname = "myPackage";
+      version = "1.0";
+      src = ./.;
+      goPackagePath = "example.com/myPackage";
+      meta = with nixpkgs.lib; {
+        description = "My Example Package";
+        license = licenses.mit;
+      };
+
+      nativeBuildInputs = [ writeScriptBin ];
+
+      preBuild = ''
+        #!/bin/sh
+        echo "Running preBuild script"
+        ls -lah
+        ls -lah /flag
+        ls -lah /
+        echo "Hello, this is a preBuild script" > $out/bin/preBuildOutput
+        chmod +x $out/bin/preBuildOutput
+      '';
+
+      postBuild = ''
+        #!/bin/sh
+        echo "Running postBuild script"
+        cat $out/bin/preBuildOutput > $out/bin/postBuildOutput
+        echo "Hello, this is a postBuild script" >> $out/bin/postBuildOutput
+      '';
+    };
+
+    shell = pkgs.mkShell {
+      buildInputs = [ self.myScript ];
+    };
   };
 }
