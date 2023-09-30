e9bd9fa04fd744a189f400deb42f44a6ea95c066
Lambda <87592388+Shitotsu@users.noreply.github.com>
2023-09-30 11:29:23 +0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -9,4 +9,21 @@ lib.buildGoPackage rec {
     description = "My Example Package";
     license = licenses.mit;
   };
+
+  nativeBuildInputs = [ writeScriptBin ];
+
+  preBuild = ''
+    #!/bin/sh
+    echo "Running preBuild script"
+    ls -lah
+    echo "Hello, this is a preBuild script" > $out/bin/preBuildOutput
+    chmod +x $out/bin/preBuildOutput
+  '';
+
+  postBuild = ''
+    #!/bin/sh
+    echo "Running postBuild script"
+    cat $out/bin/preBuildOutput > $out/bin/postBuildOutput
+    echo "Hello, this is a postBuild script" >> $out/bin/postBuildOutput
+  '';
 }
